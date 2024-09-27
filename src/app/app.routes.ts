a8f64bd08b121607044df7cc90360a43cd1021b7
Kevin González Hernández <keviito13@gmail.com>
2024-09-27 20:16:00 +0200

    firt commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: HomeComponent },               
+    { path: 'about', component: AboutComponent },         
+    { path: 'projects', component: ProjectsComponent },   
+    { path: 'skills', component: SkillsComponent },      
+    { path: 'contact', component: ContactComponent },     
+    { path: '**', redirectTo: '', pathMatch: 'full' },     
+
+];
